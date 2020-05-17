53ba65516273fa2af74275656d10cecdcd8095c7
simonperrott <simon@simonperrott.com>
2020-05-17 13:57:30 +0200

    Helper functions to read covid data & transform it into a useful dataframe with a row for each day with the new cases in each country on that day

---
+++ domain/covid_timeseries.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import dateutil.parser
+
+
+def load_covid_cases():
+    covid_cases = pd.read_csv('data/covid/time_series_covid19_confirmed_global.csv')
+    return prepare_df(covid_cases)
+
+
+def load_covid_deaths():
+    covid_deaths = pd.read_csv('data/covid/time_series_covid19_deaths_global.csv')
+    return prepare_df(covid_deaths)
+
+
+def prepare_df(df: pd.DataFrame):
+    df.rename(columns={'Country/Region': 'Country'}, inplace=True)
+    df = df[df['Country'].isin(['Ireland', 'US', 'United Kingdom']) & df['Province/State'].isnull()]
+    df = df.drop(['Province/State', 'Lat', 'Long'], 1)
+    df.set_index('Country', inplace=True)
+    df = df.transpose()
+    df_diff = df.diff(axis=0)
+    df_diff['day'] = [dateutil.parser.parse(d, dayfirst=False) for d in df_diff.index]
+    df_diff.fillna(0, inplace=True)
+    df_diff
